1816bb4db15159bbb10ddea86268396452caed4a
ahallam <alexhallam75@gmail.com>
2019-09-13 13:37:35 -0400

    Virtual field

--- models/user.js
+++ models/user.js
@@ -36,3 +36,32 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// virtual field
+userSchema
+  .virtual("password")
+  .set(function(password) {
+    this._password = password;
+    this.salt = uuidv1();
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function() {
+    return this._password;
+  });
+
+userSchema.methods = {
+  authenticate: function(plainText) {
+  return encryptPassword: function(password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  }
+};
+
+module.exports = mongoose.model("User", userSchema);
