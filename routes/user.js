01c4092c89769be0b8197c1ddbca16c7385c20c5
ahallam <alexhallam75@gmail.com>
2019-09-16 16:27:57 -0400

    signout working

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { signup, signin } = require("../controllers/user.js");
+const { signup, signin, signout } = require("../controllers/user.js");
 const { userSignupValidator } = require("../validator");
 
 router.post("/signup", userSignupValidator, signup);
 router.post("/signin", signin);
+router.get("/signout", signout);
 
 module.exports = router;
