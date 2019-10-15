515f787d74e1cf941ef10e1fad9c61b2635839ef
ahallam <alexhallam75@gmail.com>
2019-10-15 13:00:48 -0400

    product validation

--- controllers/product.js
+++ controllers/product.js
@@ -14,9 +14,34 @@ exports.create = (req, res) => {
       });
     }
 
+    // check for all fields
+    const { name, description, price, category, quantity, shipping } = fields;
+
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !category ||
+      !quantity ||
+      !shipping
+    ) {
+      return res.status(400).json({
+        error: "All fields are required"
+      });
+    }
+
     let product = new Product(fields);
 
+    // 1kb = 1000
+    // 1mb = 1000000
+
     if (files.photo) {
+      // console.log("FILES PHOTO: ", files.photo);
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: "Image should be less than 1mb in size"
+        });
+      }
       product.photo.data = fs.readFileSync(files.photo.path);
       product.photo.contentType = files.photo.type;
     }
