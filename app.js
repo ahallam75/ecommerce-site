b3b1c35db3f940ee033a6d1cf2bdaf1715e9dc1f
ahallam <alexhallam75@gmail.com>
2019-10-16 12:50:13 -0400

    cors installed

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const expressValidator = require("express-validator");
 
 require("dotenv").config();
@@ -30,6 +31,7 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 
 // Routes Middleware
 app.use("/api", authRoutes);
