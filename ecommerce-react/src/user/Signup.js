796baf2ed1fc11583a53bbf655aa36e943903359
ahallam <alexhallam75@gmail.com>
2019-10-21 14:48:43 -0400

    Environment variables

--- ecommerce-react/src/user/Signup.js
+++ ecommerce-react/src/user/Signup.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Layout from "../core/Layout";
+import { API } from "../config";
 
 const Signup = () => (
   <Layout title="Signup" description="Signup for Node-React E-Commerce App">
-    ...
+    {API}
   </Layout>
 );
 
