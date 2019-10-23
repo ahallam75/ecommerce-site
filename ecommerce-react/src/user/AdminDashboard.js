89233462b38cf796075a7d76abe2fe7ac3eff63f
ahallam <alexhallam75@gmail.com>
2019-10-23 12:35:23 -0400

    Still working on admin and user routes and dashboard

--- ecommerce-react/src/user/AdminDashboard.js
+++ ecommerce-react/src/user/AdminDashboard.js
@@ -3,24 +3,29 @@ import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
 
-const Dashboard = () => {
+const AdminDashboard = () => {
   const {
     user: { _id, name, email, role }
   } = isAuthenticated();
 
-  const userLinks = () => {
+  const adminLinks = () => {
     return (
       <div className="card">
-        <h4 className="card-header">User Links</h4>
+        <h4 className="card-header">Admin Links</h4>
         <ul className="list-group">
           <li className="list-group-item">
-            <Link className="nav-link" to="/cart">
-              My Cart
+            <Link className="nav-link" to="/create/category">
+              Create Category
             </Link>
           </li>
           <li className="list-group-item">
-            <Link className="nav-link" to="/profile/update">
-              Update Profile
+            <Link className="nav-link" to="/create/product">
+              Create Product
+            </Link>
+          </li>
+          <li className="list-group-item">
+            <Link className="nav-link" to="/admin/orders">
+              View Orders
             </Link>
           </li>
         </ul>
@@ -28,7 +33,7 @@ const Dashboard = () => {
     );
   };
 
-  const userInfo = () => {
+  const adminInfo = () => {
     return (
       <div className="card mb-5">
         <h3 className="card-header">User Information</h3>
@@ -43,17 +48,6 @@ const Dashboard = () => {
     );
   };
 
-  const purchaseHistory = () => {
-    return (
-      <div className="card mb-5">
-        <h3 className="card-header">Purchase history</h3>
-        <ul className="list-group">
-          <li className="list-group-item">history</li>
-        </ul>
-      </div>
-    );
-  };
-
   return (
     <Layout
       title="Dashboard"
@@ -61,14 +55,11 @@ const Dashboard = () => {
       className="container-fluid"
     >
       <div className="row">
-        <div className="col-3">{userLinks()}</div>
-        <div className="col-9">
-          {userInfo()}
-          {purchaseHistory()}
-        </div>
+        <div className="col-3">{adminLinks()}</div>
+        <div className="col-9">{adminInfo()}</div>
       </div>
     </Layout>
   );
 };
 
-export default Dashboard;
+export default AdminDashboard;
