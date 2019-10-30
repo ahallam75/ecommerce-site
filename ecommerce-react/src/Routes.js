8319786fa6969fa862c604d3e9aee56cc831e796
ahallam <alexhallam75@gmail.com>
2019-10-30 11:05:31 -0400

    Still working on Shop page

--- ecommerce-react/src/Routes.js
+++ ecommerce-react/src/Routes.js
@@ -9,12 +9,14 @@ import AdminRoute from "./auth/AdminRoute";
 import AdminDashboard from "./user/AdminDashboard";
 import AddCategory from "./admin/AddCategory";
 import AddProduct from "./admin/AddProduct";
+import Shop from "./core/Shop";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/shop" exact component={Shop} />
         <Route path="/Signin" exact component={Signin} />
         <Route path="/Signup" exact component={Signup} />
         <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
---
+++ ecommerce-react/src/core/Shop.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from "react";
+import Layout from "./Layout";
+import Card from "./Card";
+import { getCategories } from "./apiCore";
+import Checkbox from "./Checkbox";
+import RadioBox from "./RadioBox";
+import { prices } from "./fixedPrices";
+
+const Shop = () => {
+  const [myFilters, setMyFilters] = useState({
+    filters: { category: [], price: [] }
+  });
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState(false);
+  // const [limit, setLimit] = useState(6);
+  // const [skip, setSkip] = useState(0);
+  // const [size, setSize] = useState(0);
+  // const [filteredResults, setFilteredResults] = useState([]);
+
+  const init = () => {
+    getCategories().then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setCategories(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const handleFilters = (filters, filterBy) => {
+    // console.log("SHOP", filters, filterBy);
+    const newFilters = { ...myFilters };
+    newFilters.filters[filterBy] = filters;
+
+    if (filterBy === "price") {
+      let priceValues = handlePrice(filters);
+      newFilters.filters[filterBy] = priceValues;
+    }
+    loadFilteredResults(myFilters.filters);
+    setMyFilters(newFilters);
+  };
+
+  return (
+    <Layout
+      title="Shop Page"
+      description="Search and find books of your choice"
+      className="container-fluid"
+    >
+      <div className="row">
+        <div className="col-4">
+          <h4>Filter by categories</h4>
+          <ul>
+            <Checkbox
+              categories={categories}
+              handleFilters={filters => handleFilters(filters, "category")}
+            />
+          </ul>
+
+          <h4>Filter by price range</h4>
+          <div>
+            <RadioBox
+              prices={prices}
+              handleFilters={filters => handleFilters(filters, "price")}
+            />
+          </div>
+        </div>
+
+        <div className="col-8">
+          <h2 className="mb-4">Products</h2>
+          <div className="row">
+            {filteredResults.map((product, i) => (
+              <div key={i} className="col-4 mb-3">
+                <Card product={product} />
+              </div>
+            ))}
+          </div>
+          <hr />
+          {loadMoreButton()}
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Shop;
