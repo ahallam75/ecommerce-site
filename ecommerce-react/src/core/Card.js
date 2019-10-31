f1133f875c5766d44c94e785f01c1c37bdd9477d
ahallam <alexhallam75@gmail.com>
2019-10-31 15:05:07 -0400

    Products in cart, show/hide add to cart, quantity update, remove, and total

--- ecommerce-react/src/core/Card.js
+++ ecommerce-react/src/core/Card.js
@@ -2,10 +2,17 @@ import React, { useState } from "react";
 import { Link, Redirect } from "react-router-dom";
 import ShowImage from "./ShowImage";
 import moment from "moment";
-import { addItem } from "./cartHelpers";
+import { addItem, updateItem, removeItem } from "./cartHelpers";
 
-const Card = ({ product, showViewProductButton = true }) => {
+const Card = ({
+  product,
+  showViewProductButton = true,
+  showAddToCartButton = true,
+  cartUpdate = false,
+  showRemoveProductButton = false
+}) => {
   const [redirect, setRedirect] = useState(false);
+  const [count, setCount] = useState(product.count);
 
   const showViewButton = showViewProductButton => {
     return (
@@ -44,11 +51,51 @@ const Card = ({ product, showViewProductButton = true }) => {
     );
   };
 
+  const showRemoveButton = showRemoveProductButton => {
+    return (
+      showRemoveProductButton && (
+        <button
+          onClick={() => removeItem(product._id)}
+          className="btn btn-outline-danger mt-2 mb-2"
+        >
+          Remove Product
+        </button>
+      )
+    );
+  };
+
   const showStock = quantity => {
     return quantity > 0 ? (
       <span className="badge badge-primary badge-pill">In Stock</span>
     ) : (
       <span className="badge badge-primary badge-pill">Out of Stock</span>
+    );
+  };
+
+  const handleChange = productId => event => {
+    setCount(event.target.value < 1 ? 1 : event.target.value);
+    if (event.target.value >= 1) {
+      updateItem(productId, event.target.value);
+    }
+  };
+
+  const showCartUpdateOptions = cartUpdate => {
+    return (
+      cartUpdate && (
+        <div>
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text">Adjust Quantity</span>
+            </div>
+            <input
+              type="number"
+              className="form-control"
+              value={count}
+              onChange={handleChange(product._id)}
+            />
+          </div>
+        </div>
+      )
     );
   };
 
@@ -69,6 +116,9 @@ const Card = ({ product, showViewProductButton = true }) => {
         {showStock(product.quantity)}
         <br />
         {showViewButton(showViewProductButton)}
+        {showAddToCart(showAddToCartButton)}
+        {showRemoveButton(showRemoveProductButton)}
+        {showCartUpdateOptions(cartUpdate)}
         <button className="btn btn-outline-warning mt-2 mb-2">
           View Product
         </button>
--- ecommerce-react/src/core/cartHelpers.js
+++ ecommerce-react/src/core/cartHelpers.js
@@ -35,3 +35,47 @@ export const itemTotal = () => {
   }
   return 0;
 };
+
+export const getCart = () => {
+  if (typeof window !== "undefined") {
+    if (localStorage.getItem("cart")) {
+      return JSON.parse(localStorage.getItem("cart"));
+    }
+  }
+  return [];
+};
+
+export const updateItem = (productId, count) => {
+  let cart = [];
+  if (typeof window !== "undefined") {
+    if (localStorage.getItem("cart")) {
+      cart = JSON.parse(localStorage.getItem("cart"));
+    }
+
+    cart.map((product, i) => {
+      if (product._id === productId) {
+        cart[i].count = count;
+      }
+    });
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+};
+
+export const removeItem = productId => {
+  let cart = [];
+  if (typeof window !== "undefined") {
+    if (localStorage.getItem("cart")) {
+      cart = JSON.parse(localStorage.getItem("cart"));
+    }
+
+    cart.map((product, i) => {
+      if (product._id === productId) {
+        cart.splice(i, 1);
+      }
+    });
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+  return cart;
+};
