e15544a7f4acc8e22cad3bd17978882461274d9f
ahallam <alexhallam75@gmail.com>
2019-11-05 11:44:54 -0500

    Order product details and enum status values for orders

--- ecommerce-react/src/admin/Orders.js
+++ ecommerce-react/src/admin/Orders.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from "react";
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
-import { listOrders } from "./apiAdmin";
+import { listOrders, getStatusValues } from "./apiAdmin";
 import moment from "moment";
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
+  const [statusValues, setStatusValues] = useState([]);
 
   const { user, token } = isAuthenticated();
 
@@ -20,8 +21,19 @@ const Orders = () => {
     });
   };
 
+  const loadStatusValues = () => {
+    getStatusValues(user._id, token).then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setStatusValues(data);
+      }
+    });
+  };
+
   useEffect(() => {
     loadOrders();
+    loadStatusValues();
   }, []);
 
   const showOrdersLength = () => {
@@ -32,6 +44,41 @@ const Orders = () => {
     } else {
       return <h1 className="text-danger">No orders</h1>;
     }
+  };
+
+  showInput = (key, value) => {
+    <div className="input-group mb-2 mr-sm-2">
+      <div className="input-group prepend">
+        <div className="input-group-text">{key}</div>
+      </div>
+      <input
+        type="text"
+        value={value}
+        className="form-control"
+        readOnly
+      ></input>
+    </div>;
+  };
+
+  const handleStatusChange = (e, orderId) => {
+    console.log("update order status");
+  };
+
+  const showStatus = () => {
+    <div className="form-group">
+      <h3 className="mark mb-4">Status: {o.status}</h3>
+      <select
+        className="form-control"
+        onChange={e => handleStatusChange(e, o._id)}
+      >
+        <option>Update Status</option>
+        {statusValues.map((status, index) => (
+          <option key={index} value={status}>
+            {status}
+          </option>
+        ))}
+      </select>
+    </div>;
   };
 
   return (
@@ -55,7 +102,7 @@ const Orders = () => {
                 </h2>
 
                 <ul className="list-group mb-2">
-                  <li className="list-group-item">{o.status}</li>
+                  <li className="list-group-item">{showStatus.o}</li>
                   <li className="list-group-item">
                     Transaction ID: {o.transaction_id}
                   </li>
@@ -72,6 +119,18 @@ const Orders = () => {
                 <h3 className="mt-4 mb-4 font-italic">
                   Total products in the order: {o.products.length}
                 </h3>
+                {o.products.map((p, pIndex) => (
+                  <div
+                    className="mb-4"
+                    key={pIndex}
+                    style={{ padding: "20px", border: "1px solid indigo" }}
+                  >
+                    {showInput("Product name", p.name)}
+                    {showInput("Product price", p.price)}
+                    {showInput("Product total", p.count)}
+                    {showInput("Product Id", p._id)}
+                  </div>
+                ))}
               </div>
             );
           })}
