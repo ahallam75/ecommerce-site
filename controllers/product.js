218abd37de43ae8508c75c4252f472440bb1b766
ahallam <alexhallam75@gmail.com>
2019-11-05 14:45:59 -0500

    Get all products for admin CRUD and product update

--- controllers/product.js
+++ controllers/product.js
@@ -98,21 +98,6 @@ exports.update = (req, res) => {
         error: "Image could not be uploaded"
       });
     }
-    // check for all fields
-    const { name, description, price, category, quantity, shipping } = fields;
-
-    if (
-      !name ||
-      !description ||
-      !price ||
-      !category ||
-      !quantity ||
-      !shipping
-    ) {
-      return res.status(400).json({
-        error: "All fields are required"
-      });
-    }
 
     let product = req.product;
     product = _.extend(product, fields);
