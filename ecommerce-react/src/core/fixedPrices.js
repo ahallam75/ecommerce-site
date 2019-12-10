056f474d5e7936953e350e8574eaa359cc2e0a0b
ahallam <alexhallam75@gmail.com>
2019-12-10 17:14:09 -0500

    Fixed price now working

--- ecommerce-react/src/core/fixedPrices.js
+++ ecommerce-react/src/core/fixedPrices.js
@@ -6,23 +6,23 @@ export const prices = [
   },
   {
     _id: 1,
-    name: "$0 to $9",
-    array: [0, 9]
+    name: "$0 to $9.99",
+    array: [0, 9.99]
   },
   {
     _id: 2,
-    name: "$10 to $19",
-    array: [10, 19]
+    name: "$10 to $19.99",
+    array: [10, 19.99]
   },
   {
     _id: 3,
-    name: "$20 to $29",
-    array: [20, 29]
+    name: "$20 to $29.99",
+    array: [20, 29.99]
   },
   {
     _id: 4,
-    name: "$30 to $39",
-    array: [20, 29]
+    name: "$30 to $39.99",
+    array: [30, 39.99]
   },
   {
     _id: 5,
